ea533dd506a40dcc658623cc6be4b716003428fb
quan27318 <88961258+quan27318@users.noreply.github.com>
2022-02-21 20:37:49 +0700

    Add files via upload

---
+++ WebAPI/Program.cs
@@ -0,0 +1,28 @@
+using WebAPI.Models;
+using WebAPI.Interfaces;
+using WebAPI.Implement;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IService, TaskService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
